e052211c023932b4ddbd7c346a55a8b8cc5bd478
alex.hyman@gmail.com <alex.hyman@gmail.com>
2018-07-13 09:55:51 -0500

    added comments to the file

--- pay.py
+++ pay.py
@@ -1,3 +1,11 @@
+# File name: pay.py
+# Create on: 13 Jul 2018
+# Created By: Alex Hyman
+# Purpose: Take user inputs for rate and hours and calculate the amount
+# of money they had made for the week
+#
+#
+#
 hrs = float(input("Enter hours: "))
 rate = float(input("Enter rate: "))
 print("Pay: {:.2f}".format(hrs * rate))
