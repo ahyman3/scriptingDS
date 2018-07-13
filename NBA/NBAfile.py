06cef40f534d7931578c5fea2b2fa3c1ea16d1d9
alex.hyman@gmail.com <alex.hyman@gmail.com>
2018-07-13 10:53:44 -0500

    Created the file

---
+++ NBA/NBAfile.py
@@ -0,0 +1,5 @@
+# File Name: NBAfile.py
+# Created By: Alex Hyman
+# Created on: 13 Jul 2018
+# Purpose: To read the nba attendance file and print out a string for
+# each city that says the average attendance and the average ticket price
